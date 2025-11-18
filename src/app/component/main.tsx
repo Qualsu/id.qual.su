803f12ad605b66fb494b085889d70b9856d1108e
ItzKeeni <maratkanovartem89@mail.ru>
2025-11-18 17:31:19 +0300

    Change banner

--- src/app/component/main.tsx
+++ src/app/component/main.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import bannerImg from "../img/Banner.png"
-import idImg from "../img/ID.svg"
+import idImg from "../img/QualID.svg"
 
 export function Main(){
     return (
@@ -9,14 +9,7 @@ export function Main(){
             <Image src={bannerImg} width="1000" alt="banner" className="banner animate-fade animate-duration-[700ms] animate-delay-[1000ms]"/>
           </div>
           <h1 className="flex items-center md:flex-row flex-col justify-center text-3xl break-words p-4 text-white animate-fade-up animate-once animate-duration-[1300ms] animate-delay-[1300ms]">
-              <span className="text-qual-Q flex items-center mb-2 md:mb-0">
-                Qu
-                <span className="text-qual-U">
-                  al
-                </span>
-                <Image src={idImg} width="30" alt="ID" className="mx-2"/>
-              </span>
-              
+              <Image src={idImg} width="130" alt="ID" className="mx-2"/>
               <span className="mr-2"> - </span>
               Единый аккаунт для всех
           </h1>
